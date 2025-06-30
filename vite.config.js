9e40e0a612f4b927bcd7af47d3b882920aab141d
martin <martin_pan2345@gmail.com>
2025-06-30 09:54:59 +0800

    fix build

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import svgr from "vite-plugin-svgr";
 export default defineConfig({
   plugins: [react(), tailwindcss(), svgr()],
   build: {
-    outDir: "../docs",
+    outDir: "./docs",
     emptyOutDir: true, // also necessary
   },
 });
